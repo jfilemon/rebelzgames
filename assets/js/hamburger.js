1e9c8108dcd1d3ea467cbbb2862f011f4264909e
jfilemon <j.vormgever@gmail.com>
2015-10-20 18:14:53 +0200

    V 1.0

--- assets/js/hamburger.js
+++ assets/js/hamburger.js
@@ -1,3 +1,38 @@
-$('button').click(function() {
-  $(this).toggleClass('expanded').siblings('div').slideToggle();
+
+$(function() {
+  
+  initDropDowns($("div.shy-menu"));
+
 });
+
+function initDropDowns(allMenus) {
+
+  allMenus.children(".shy-menu-hamburger").on("click", function() {
+    
+    var thisTrigger = jQuery(this),
+      thisMenu = thisTrigger.parent(),
+      thisPanel = thisTrigger.next();
+
+    if (thisMenu.hasClass("is-open")) {
+
+          thisMenu.removeClass("is-open");
+      
+          jQuery(document).off("click");                                 
+          thisPanel.off("click");
+
+    } else {      
+      
+        allMenus.removeClass("is-open");  
+      thisMenu.addClass("is-open");
+  
+      jQuery(document).on("click", function() {
+        allMenus.removeClass("is-open");
+      });
+      thisPanel.on("click", function(e) {
+        e.stopPropagation();
+      });
+    }
+    
+    return false;
+  });
+}
